287faedd3641467a1e0837742400f41e5938a798
ShotaAyabe <a.s.1030.eren21s@gmail.com>
2022-12-12 18:24:52 +0900

    デプロイテスト

--- resources/views/tweets/create.blade.php
+++ resources/views/tweets/create.blade.php
@@ -36,7 +36,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-12 text-right">
-                                <p class="mb-4 text-danger">140文字以内</p>
+                                <p class="mb-4 text-danger">140字以内</p>
                                 <button type="submit" class="btn btn-primary">
                                     ツイートする
                                 </button>
