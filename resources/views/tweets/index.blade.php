75732ffb3fb03ce838075be79bfa185a082160eb
ShotaAyabe <a.s.1030.eren21s@gmail.com>
2022-12-17 16:26:35 +0900

    update

--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        
  <!--動画-->
         @if (isset($timelines))
             @foreach ($timelines as $timeline)
