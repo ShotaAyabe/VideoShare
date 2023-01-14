c9751dcbd117e3839fbbd59213f41db04758daf5
ShotaAyabe <a.s.1030.eren21s@gmail.com>
2023-01-15 00:28:57 +0900

    “feat:bladeの調整”

--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -41,7 +41,7 @@
                                         <!--投稿日時を表示する-->    
                                             <div class="card-author text-muted">
                                                 <i class="fa fa-clock-o m-r-xs"></i>
-                                                2015/08/14
+                                                2000/00/00
                                             </div>
                                         </div>
                                     </div>
