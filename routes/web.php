347c44c224c916ac2ba20aa4c8c40c4473d00831
ShotaAyabe <a.s.1030.eren21s@gmail.com>
2023-01-16 13:44:26 +0900

    “feat:コードの微調整”

--- routes/web.php
+++ routes/web.php
@@ -24,23 +24,16 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 // ログイン状態
 Route::group(['middleware' => 'auth'], function() {
-
     // ユーザ関連
     Route::resource('users', 'UsersController', ['only' => ['index', 'show', 'edit', 'update']]);
-
     // フォロー/フォロー解除を追加
     Route::post('users/{user}/follow', 'UsersController@follow')->name('follow');
     Route::delete('users/{user}/unfollow', 'UsersController@unfollow')->name('unfollow');
-
     // ツイート関連
     Route::resource('tweets', 'TweetsController', ['only' => ['index', 'create', 'store', 'show', 'edit', 'update', 'destroy']]);
-    
-    
     // コメント関連
     Route::resource('comments', 'CommentsController', ['only' => ['store']]);
-    
     // いいね関連
     Route::resource('favorites', 'FavoritesController', ['only' => ['store', 'destroy']]);
     
-
 });
