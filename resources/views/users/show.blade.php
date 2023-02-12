b1a2bde849d41418522641138e8131f2ed4d5f23
ShotaAyabe <a.s.1030.eren21s@gmail.com>
2023-02-12 14:59:56 +0900

    feat:ユーザー詳細画面に動画ボタンを実装

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -58,14 +58,14 @@
         </div>
         
         
-        
+ <!--動画-->
         @if (isset($timelines))
             @foreach ($timelines as $timeline)
                 <div class="col-md-8 mb-3">
                     <div class="card">
                         <div class="card-haeder p-3 w-100 d-flex">
-                            <img src="{{ $user->profile_image }}" class="rounded-circle" width="50" height="50">
-                            <div class="ml-2 d-flex flex-column flex-grow-1">
+                            <img src="{{ $timeline->user->profile_image }}" class="rounded-circle" width="50" height="50">
+                            <div class="ml-2 d-flex flex-column">
                                 <p class="mb-0">{{ $timeline->user->name }}</p>
                                 <a href="{{ url('users/' .$timeline->user->id) }}" class="text-secondary">{{ $timeline->user->screen_name }}</a>
                             </div>
@@ -74,7 +74,37 @@
                             </div>
                         </div>
                         <div class="card-body">
-                            {{ $timeline->text }}
+ {{--                        
+                            <a href="{{ $timeline->url }}">
+                            {!! nl2br(e($timeline->url)) !!}
+                            </a>
+--}}
+                            <a href="" class="card card-w-image rounded-0 open-player disabled" data-url="{{ $timeline->url }}" data-start="0" data-end="11812" data-fname="scare jump ur whalecome" style="margin-bottom: 10px;">
+                                <div class="row no-gutters">
+                                    <div class="col-4 col-md-2">
+                                        <!--サムネイルを表示する-->
+                                        <div class="card-thumbnail">
+                                            <img src="" class="img-fluid" alt="" style="padding: 3px;">
+                                        </div>
+                                    </div>
+                                    <div class="col-8 col-md-10">
+                                        <div class="card-body">
+                                         <!--タイトル-->
+                                            <h3 class="card-heading mb0" style="overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">YouTubeテーマソング／ヒカキン＆セイキン</h3>
+                                        <!--チャンネル名を表示させたい-->
+                                            <div class="card-author text-muted" style="overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">HikakinTV</div>
+                                        <!--投稿日時を表示する-->    
+                                            <div class="card-author text-muted">
+                                                <i class="fa fa-clock-o m-r-xs"></i>
+                                                 2000/00/00
+                                                 <!--投稿日時を表示したいか、共有日時を表示したいかを考える-->
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                            <br>
+                            {!! nl2br(e($timeline->text)) !!}
                         </div>
                         <div class="card-footer py-1 d-flex justify-content-end bg-white">
                             @if ($timeline->user->id === Auth::user()->id)
@@ -83,10 +113,10 @@
                                         <i class="fas fa-ellipsis-v fa-fw"></i>
                                     </a>
                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                        <a href="{{ url('tweets/' .$timeline->id .'/edit') }}" class="dropdown-item">編集</a>
                                         <form method="POST" action="{{ url('tweets/' .$timeline->id) }}" class="mb-0">
                                             @csrf
                                             @method('DELETE')
-                                            <a href="{{ url('tweets/' .$timeline->id .'/edit') }}" class="dropdown-item">編集</a>
                                             <button type="submit" class="dropdown-item del-btn">削除</button>
                                         </form>
                                     </div>
@@ -96,16 +126,16 @@
                                 <a href="{{ url('tweets/' .$timeline->id) }}"><i class="far fa-comment fa-fw"></i></a>
                                 <p class="mb-0 text-secondary">{{ count($timeline->comments) }}</p>
                             </div>
+ {{--いいね処理追記--}}                            
                             <div class="d-flex align-items-center">
-                                @if (!in_array(Auth::user()->id, array_column($timeline->favorites->toArray(), 'user_id'), TRUE))
+                                @if (!in_array($user->id, array_column($timeline->favorites->toArray(), 'user_id'), TRUE))
                                     <form method="POST" action="{{ url('favorites/') }}" class="mb-0">
                                         @csrf
- {{--いいね処理--}}
                                         <input type="hidden" name="tweet_id" value="{{ $timeline->id }}">
                                         <button type="submit" class="btn p-0 border-0 text-primary"><i class="far fa-heart fa-fw"></i></button>
                                     </form>
                                 @else
-                                    <form method="POST"action="{{ url('favorites/' .array_column($timeline->favorites->toArray(), 'id', 'user_id')[Auth::user()->id]) }}" class="mb-0">
+                                    <form method="POST" action="{{ url('favorites/' .array_column($timeline->favorites->toArray(), 'id', 'user_id')[$user->id]) }}" class="mb-0">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" class="btn p-0 border-0 text-danger"><i class="fas fa-heart fa-fw"></i></button>
@@ -113,15 +143,32 @@
                                 @endif
                                 <p class="mb-0 text-secondary">{{ count($timeline->favorites) }}</p>
                             </div>
- {{--いいね処理--}}
+{{--いいね処理追記--}}                            
                         </div>
                     </div>
                 </div>
             @endforeach
         @endif
+        
     </div>
     <div class="my-4 d-flex justify-content-center">
         {{ $timelines->links() }}
     </div>
+<!--　必要だった部分　-->
+<!--　動画プレイヤーの部分　-->
+        <div class="player">
+            <!-- プレイヤー部分 -->
+            <div id="youtube" class="iframe"></div>
+            
+            <!-- 動画の閉じるボタン -->
+            <div class="controller">
+                <div class="close-player" title="プレイヤーを閉じる">
+                    <i class="fa fa-2x fa-times-circle"></i>
+                </div>
+            </div>
+            <!-- なくても良さそう -->
+            <!--<div class="reclist"></div>-->
+        </div>
+    
 </div>
 @endsection
