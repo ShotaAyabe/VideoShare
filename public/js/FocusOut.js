718694ee5f39e48c479fa3cd8f90eff74de7ea28
ShotaAyabe <a.s.1030.eren21s@gmail.com>
2023-02-19 18:33:00 +0900

    feat:Added processing to determine if the string entered in the URL field is a URL when input is complete.

---
+++ public/js/FocusOut.js
@@ -0,0 +1,31 @@
+//Detect focus out
+//Detect input errors
+$(function(){
+	$('[name="url"]').on('blur', function(event){
+		const baseUrl = [
+			"https://youtu.be/",
+			"https://www.youtube.com/"
+		];
+		
+		const target = $(this);
+		const errLabel = $('[name="error"]'); 
+		let isValid = false;
+
+		for(const url of baseUrl){
+			if(target.val().startsWith(url)){
+				isValid = true;
+			}
+		}
+
+		target.removeClass("invalid");
+		errLabel.text("");
+
+		if(!isValid){
+			target.addClass("invalid");
+			errLabel.text("正しいYouTubeのURLが入力されていません。");
+			return;
+		}
+		// TODO:情報取得
+		//SearchVideoDetails
+	});
+})
--- resources/views/tweets/create.blade.php
+++ resources/views/tweets/create.blade.php
@@ -1,16 +1,13 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Create</div>
-
                 <div class="card-body">
-                    <form method="POST" action="{{ route('tweets.store') }}">
+                    <form method="POST" action="{{ route('tweets.store') }}" name="form1">
                         @csrf
-
                         <div class="form-group row mb-0">
                             <div class="col-md-12 p-3 w-100 d-flex">
                                 <img src="{{ $user->profile_image }}" class="rounded-circle" width="50" height="50">
@@ -20,12 +17,8 @@
                                 </div>
                             </div>
                             <div class="col-md-12">
-                               
                                 <textarea class="form-control @error('url') is-invalid @enderror" name="url" required autocomplete="url" rows="1">{{ old('url') }}</textarea>
-                               
                                 <textarea class="form-control @error('text') is-invalid @enderror" name="text" required autocomplete="text" rows="4">{{ old('text') }}</textarea>
-
-
                                 @error('text')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -33,9 +26,11 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group row mb-0">
                             <div class="col-md-12 text-right">
+
+                                <div name="error"></div>
+                                
                                 <p class="mb-4 text-danger">140字以内</p>
                                 <button type="submit" class="btn btn-primary">
                                     動画を共有する
